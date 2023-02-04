7e0f66cd7c4f554584b81c0b1e2ffad4abe08666
Vika <vika.tulnikova@bk.ru>
2023-02-04 20:36:10 +0300

    add task1

---
+++ task1/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Введите натуральное число больше 1: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+void FindNumber (int currIndex, int n)
+{
+    if (currIndex == n) 
+        return;
+    
+    Console.Write("{0,4}", currIndex);
+    FindNumber (--currIndex, n);
+}
+
+FindNumber(number, 0);
