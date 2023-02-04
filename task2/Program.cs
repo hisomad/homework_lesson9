59e3f6c0448ebbdde0725eef95f2e5140ad17d9e
Vika <vika.tulnikova@bk.ru>
2023-02-04 20:40:16 +0300

    add task2

---
+++ task2/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.Write("Введите значение M: ");
+int m = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите значение N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+void SumOfNumbers (int m, int n, int result)
+{
+    if (m > n)
+    {
+        System.Console.WriteLine($"Сумма натуральных чисел в промежутке от M до N -> {result}");
+            return;
+    }
+    
+    result = result + (m++);
+    SumOfNumbers(m, n, result);
+}
+
+SumOfNumbers(m, n, 0);
